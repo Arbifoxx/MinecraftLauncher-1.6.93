d391f687e446a7eab54d5c8ae10e0d5ec49a8337
Brandon Lekai <brandon.lekai@gmail.com>
2024-04-20 14:36:55 -0500

    Modify clean.sh and fix redist generation. Add compile.sh for easy compilation.

--- clean.sh
+++ clean.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-rm -rf work
-rm -rf launcher olauncher
-rm olauncher-*-redist.jar
+printf 'Do you want to remove AutoOL? It will be redownloaded when compile.sh is run again. (y/n)? '
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty $old_stty_cfg
+if [ "$answer" != "${answer#[Yy]}" ];then
+    rm -rf work
+    rm -rf launcher olauncher
+    rm MinecraftLauncher-*-redist.jar
+    rm -rf AutoOL/*
+elif [ "$answer" != "${answer#[Nn]}" ];then
+    rm -rf work
+    rm -rf launcher olauncher
+    rm MinecraftLauncher-*-redist.jar
+else
+    echo
+    echo "Answer y or n."
+fi
+
--- compile.sh
+++ compile.sh
@@ -1,5 +1,24 @@
+#Check to see if AutoOL was cloned
+if [ ! -f ./AutoOL/pom.xml ]; then
+	echo "AutoOL doesn't exist! Cloning..."
+	sleep 2
+	echo
+	rm -rf AutoOL
+	git clone https://github.com/olauncher/AutoOL
+	cd AutoOL
+	git checkout e605537cc067b4873181d9590f70394a5a32e253
+	cd ..
+fi
+echo
+echo "Done! Proceeding with compilation..."
+sleep 2
+echo
 ./decompile.sh
 ./init.sh
 ./applyPatches.sh
 mvn clean package
+echo
+echo "Successfully compiled! Generating redist jar..."
+sleep 2
+echo
 ./genredist.sh
